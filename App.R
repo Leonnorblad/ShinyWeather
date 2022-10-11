db11d043977319c8a7f6f7779bb2f7e032caa5f1
Leonard Norblad <norblad88@gmail.com>
2022-10-11 12:00:59 +0200

    packFix

--- App.R
+++ App.R
@@ -21,51 +21,51 @@ render_plot <- function(city, date, W_variable){
   title <- paste0(W_variable, ", ", data$City[1])
   
   # construct the subtitle
-  subtitle <- paste0(lubridate::day(date_of_day)," ", str_to_title(month(date_of_day, label = TRUE, abbr=FALSE), ","), " ", year(date_of_day))[1]
+  subtitle <- paste0(lubridate::day(date_of_day)," ", stringr::str_to_title(lubridate::month(date_of_day, label = TRUE, abbr=FALSE), ","), " ", lubridate::year(date_of_day))[1]
   
   # Line plots
   if (any(c("Temperature"==W_variable,
             "Humidity"==W_variable,
             "Cloudcover"==W_variable))){
-    plot <- ggplot(data=data, aes_string(x="time", y=W_variable, group=1)) + geom_line() + geom_point()
+    plot <- ggplot2::ggplot(data=data, ggplot2::aes_string(x="time", y=W_variable, group=1)) + ggplot2::geom_line() + ggplot2::geom_point()
   }
   
   # Bar plot
   if(W_variable=="Precipitation"){
-    plot <- ggplot(data=data, aes_string(x="time", y=W_variable, group = 1)) +
+    plot <- ggplot2::ggplot(data=data, ggplot2::aes_string(x="time", y=W_variable, group = 1)) +
       geom_bar(stat='identity') +
       scale_y_continuous(expand = c(0, 0)) +
-      labs(y="Precipitation (mm)")
+      ggplot2::labs(y="Precipitation (mm)")
   }
   # Vector plot
   if(W_variable=="Wind speed"){
-    plot <- ggplot(data=data, aes(x=time, y=Wind_speed)) +
+    plot <- ggplot2::ggplot(data=data, aes(x=time, y=Wind_speed)) +
       geom_text(aes(angle=-Wind_direction+90), label="-->", size=6) +
-      geom_point(size=2) + labs(y="Wind speed (m/s)")
+      geom_point(size=2) + ggplot2::labs(y="Wind speed (m/s)")
   }
   
   # Change y-labs
   if(W_variable=="Temperature"){
-    plot <- plot + labs(y="Temperature (\u00b0 C)")
+    plot <- plot + ggplot2::labs(y="Temperature (\u00b0 C)")
   }
   
   if(W_variable=="Humidity"){
-    plot <- plot + labs(y="Humidity (%)")
+    plot <- plot + ggplot2::labs(y="Humidity (%)")
   }
   
   if(W_variable=="Cloudcover"){
-    plot <- plot + labs(y="Cloudcover (%)")
+    plot <- plot + ggplot2::labs(y="Cloudcover (%)")
   }
   
   # Add theme (to all plots)
   plot <- plot +
-    labs(title = title,
+    ggplot2::labs(title = title,
          x = "Hour",
-         subtitle = subtitle) + theme_bw() +
-    theme(plot.title = element_text(face="bold", hjust=0.5, size = 16),
-          plot.subtitle = element_text(hjust=0.5),
-          axis.title = element_text(size=14),
-          axis.text = element_text(size=12))
+         subtitle = subtitle) + ggplot2::theme_bw() +
+    ggplot2::theme(plot.title = ggplot2::element_text(face="bold", hjust=0.5, size = 16),
+          plot.subtitle = ggplot2::element_text(hjust=0.5),
+          axis.title = ggplot2::element_text(size=14),
+          axis.text = ggplot2::element_text(size=12))
   
   # Change the scale for the variables in % from 0 to 100
   if(any("Humidity"==W_variable, "Cloudcover"==W_variable)){
